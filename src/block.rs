5a6303e6cfa8c9c3465c76b60d463a4586c4b55d
abel <67806187+standard3@users.noreply.github.com>
2024-11-14 16:24:21 +0100

    feat: added base node and block traits

--- src/block.rs
+++ src/block.rs
@@ -1,1 +1,41 @@
+//! To-do
+//! - [ ] Implement the `Block` trait
+//! - [ ] Implement the `CodeBlock` struct and Block trait
+//! - [ ] Implement the `DataBlock` struct and Block trait
+//! - [ ] Implement the `ProxyBlock` struct and Block trait
 
+use crate::gtirb::proto::{CodeBlock, DataBlock, Module, ProxyBlock, Symbol};
+
+use crate::node::Node;
+
+/// Block represents a base trait for blocks. [`Symbol`] objects may have references to any kind of Block.
+pub trait Block {
+    type Node: Node;
+
+    // Get the module this node ultimately belongs to
+    fn get_module(&self) -> Option<Module>;
+
+    // Get all the symbols that refer to this block
+    fn get_references(&self) -> Vec<Symbol>;
+}
+
+impl CodeBlock {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        CodeBlock::default()
+    }
+}
+
+impl DataBlock {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        DataBlock::default()
+    }
+}
+
+impl ProxyBlock {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        ProxyBlock::default()
+    }
+}
