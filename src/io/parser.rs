d6fd7a8c03d06385d67953853fb2859c4bc25714
abel <67806187+standard3@users.noreply.github.com>
2024-12-17 15:47:33 +0100

    docs: parser warning

--- src/io/parser.rs
+++ src/io/parser.rs
@@ -1,2 +1,3 @@
 //! Custom parser for consuming binaries and generating [`crate::ir::ir::Ir`] objects.
-//! Directly handles the parsing of the input binary file and generation of the IR.
+//! Directly handles the parsing of the input binary file.
+//! This is not a disassembler, it is a GTIRB parser.
